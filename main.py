b5972032eefef73e8ffd0589d081690dd0ad9e84
Neil Hendren <neil.hendren@gmail.com>
2019-02-02 23:38:26 -0700

    start

--- main.py
+++ main.py
@@ -7,8 +7,21 @@ class Window(Frame):
 
         self.master = master
 
+        self.init_window()
+
+    def init_window(self):
+
+        self.master.title("GUI")
+
+        self.pack(fill=BOTH, expand=1)
+
+        quitButton = Button(self, text="Quit")
+
+        quitButton.place(x=0, y=0)
+
 
 root = Tk()
+root.geometry("400x300")
 
 app = Window(root)
 
