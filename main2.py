b3f78beb94e2d624be73089699460b6b941e7292
Neil Hendren <neil.hendren@gmail.com>
2019-02-03 23:51:01 -0700

    save

--- main2.py
+++ main2.py
@@ -19,7 +19,7 @@ class MainProg(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne):
+        for F in (StartPage, PageOne, PageTwo):
 
             frame = F(container, self)
 
@@ -56,6 +56,26 @@ class PageOne(tk.Frame):
         button1 = tk.Button(self, text="Visit Start Page", font=SMALL_FONT,
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
+
+        button2 = tk.Button(self, text="Visit Page 2", font=SMALL_FONT,
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page 2", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Visit Start Page", font=SMALL_FONT,
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Visit Page 1", font=SMALL_FONT,
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
 
 
 app = MainProg()
